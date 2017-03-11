4bc5388403fb73b5c4b250e4630f61c15b90cc88
Szymon Pajzert <szymonpajzert@gmail.com>
2017-03-11 19:16:07 +0100

    Kinda working RunAuto - has to be tested

--- zadanie1/RunAuto.hs
+++ zadanie1/RunAuto.hs
@@ -2,28 +2,40 @@ module Main (main) where
 import Auto
 import Data.Char
 import System.Environment
+import Data.Maybe
+import Control.Exception
+import Text.Read
 
-newtype Alpha = Alpha { char :: Char } deriving (Eq)
+newtype Alpha = Alpha { unAlpha :: Char } deriving (Eq)
 instance Bounded Alpha where
   minBound = Alpha 'A'
   maxBound = Alpha 'Z'
 instance Enum Alpha where
-  fromEnum = ord . char
+  fromEnum = ord . unAlpha
   toEnum = Alpha . chr
 instance Show Alpha where
-  show = show . char
+  show = show . unAlpha
 
 -- Note: do not use newtype to opaque the type inside record
 
-data FileInput = FileInput {
-  statesNumber :: Int,
-  initStates :: [Int],
-  acceptingStates :: [Int],
-  transitions :: [(Int, [Char], [Int])],
-  word :: String
-  }
+data FileInput = FileInput { statesNumber :: Int
+                           , initStates :: [Int]
+                           , acceptingStates :: [Int]
+                           , transitions :: [(Int, [Char], [Int])]
+                           , word :: [Alpha]
+                           }
 
-parseInput :: [String] -> IO FileInput
+newtype Transition = Transition (Int, [Char], [Int])
+instance Read Transition where
+  readsPrec _ line = [(Transition (read number, chars, map read statesList), "")]
+    where number:chars:statesList = words line 
+
+parseLine :: String -> Maybe (Int, [Char], [Int])
+parseLine line = do
+  Transition transition <- readMaybe line
+  return transition
+
+parseInput :: [String] -> Maybe FileInput
 parseInput lines = do
   let _number:_initStates:_acceptingStates:_rest = lines
   let word:rest = reverse _rest
@@ -31,40 +43,43 @@ parseInput lines = do
   let number = read _number
   let initStates = rList _initStates
   let acceptingStates = rList _acceptingStates
-
+  
   return FileInput {
     statesNumber = number,
     initStates = initStates,
     acceptingStates = acceptingStates,
-    transitions = map parse rest, 
-    word = word
+    transitions = map ((\(Just x) -> x) . parseLine) rest, 
+    word = map Alpha word
     }
-    where parse line = (0, [], [0]) -- TODO implement
-
 
 readFileInput :: FilePath -> IO FileInput
 readFileInput fileName = do
   fileContent <- readFile fileName
-  fileInput <- parseInput $ lines fileContent
+  let Just fileInput = parseInput $ lines fileContent
   return fileInput
 
-createAutomata :: FileInput -> Maybe (Auto Char Int)
+createAutomata :: FileInput -> Maybe (Auto Alpha Int)
 createAutomata input =
   if validation
   then Just $ fromLists states (initStates input) (acceptingStates input) transitionList
   else Nothing
   where states = [1..(statesNumber input)]
         transitionList = func `concatMap` (transitions input)
-        func (start, chars, ends) = (\char -> (start, char, ends)) `map` chars
+        func (start, chars, ends) = (\char -> (start, Alpha char, ends)) `map` chars
         validation = True -- TODO implement
 
-
-rList :: String -> [Int]	  
+rList :: String -> [Int]
 rList = read
 
-main = do
-  fileName:[] <- getArgs
+hmain :: IO ()
+hmain = do
+  [fileName] <- getArgs
   fileInput <- readFileInput fileName
   let Just automata = createAutomata fileInput
   
-  putStrLn $ show (accepts automata (word fileInput))
+  putStrLn $ show $ accepts automata $ word fileInput
+
+main =
+  catch (hmain) (\e -> do
+      putStrLn $ show (e :: IOException)
+      putStrLn "BAD INPUT")
