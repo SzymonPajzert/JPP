c3a5153526d2fcba8f1d1b0230e89d9afee27425
Szymon Pajzert <szymonpajzert@gmail.com>
2017-04-13 14:47:30 +0200

    Lists supported

--- interpreter/src/syntax/test.sh
+++ interpreter/src/syntax/test.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
+# files good/*new have outputed their contents
+
 bnfc -m grammar.cf
 
 happy -gcad --info=gram ParGrammar.y
 alex -g LexGrammar.x
 ghc --make TestGrammar.hs -o TestGrammar
 
+rm -r res
 mkdir -p res/good
 
 fail_count=0
 
 for file in good/* ; do
-    echo "testing $file"
-	cat $file
-	echo ""
+    echo -n "testing $file:   "
+	if [[ `printf $file | tail -c 3` == "new" ]]; then
+		echo ""
+		cat $file
+ 		echo ""
+	fi
+	
 	./TestGrammar < $file &> res/$file
 	if (( $? == 0 )); then
-		echo "OK"
-		echo ""
+		echo -e "OK"
 	else
-		echo "BAD"
-		echo ""
+		echo -e "BAD"
 		fail_count=$((fail_count + 1))
 	fi
 done
 
-echo "$fail_count tests failed"
+echo -e "\n\n\n$fail_count tests failed"
 
 
