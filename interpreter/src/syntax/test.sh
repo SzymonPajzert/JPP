fedd7f043c3eca28af1e6c39cb7e1361c9ecc0f8
Szymon Pajzert <szymonpajzert@gmail.com>
2017-04-13 22:48:15 +0200

    Without arithmetics

--- interpreter/src/syntax/test.sh
+++ interpreter/src/syntax/test.sh
@@ -10,6 +10,7 @@ ghc --make TestGrammar.hs -o TestGrammar
 
 rm -r res
 mkdir -p res/good
+mkdir -p res/bad
 
 fail_count=0
 
@@ -23,6 +24,23 @@ for file in good/* ; do
 	
 	./TestGrammar < $file &> res/$file
 	if (( $? == 0 )); then
+		echo -e "OK"
+	else
+		echo -e "BAD"
+		fail_count=$((fail_count + 1))
+	fi
+done
+
+for file in bad/* ; do
+    echo -n "testing $file:   "
+	if [[ `printf $file | tail -c 3` == "new" ]]; then
+		echo ""
+		cat $file
+ 		echo ""
+	fi
+	
+	./TestGrammar < $file &> res/$file
+	if (( $? != 0 )); then
 		echo -e "OK"
 	else
 		echo -e "BAD"
